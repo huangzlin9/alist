9988dac7f72dd2dbf2e13ecb76083d3fdc366a9a
huangzlin9 <36072596+huangzlin9@users.noreply.github.com>
2025-03-19 00:02:26 +0800

    Update base.go

--- internal/task/base.go
+++ internal/task/base.go
@@ -1,90 +1,26 @@
 package task
 
 import (
-	"context"
-	"github.com/alist-org/alist/v3/internal/conf"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/xhofe/tache"
-	"sync"
-	"time"
 )
 
-type TaskExtension struct {
+type TaskWithCreator struct {
 	tache.Base
-	ctx          context.Context
-	ctxInitMutex sync.Mutex
-	Creator      *model.User
-	startTime    *time.Time
-	endTime      *time.Time
-	totalBytes   int64
+	Creator *model.User
 }
 
-func (t *TaskExtension) SetCreator(creator *model.User) {
+func (t *TaskWithCreator) SetCreator(creator *model.User) {
 	t.Creator = creator
 	t.Persist()
 }
 
-func (t *TaskExtension) GetCreator() *model.User {
+func (t *TaskWithCreator) GetCreator() *model.User {
 	return t.Creator
 }
 
-func (t *TaskExtension) SetStartTime(startTime time.Time) {
-	t.startTime = &startTime
-}
-
-func (t *TaskExtension) GetStartTime() *time.Time {
-	return t.startTime
-}
-
-func (t *TaskExtension) SetEndTime(endTime time.Time) {
-	t.endTime = &endTime
-}
-
-func (t *TaskExtension) GetEndTime() *time.Time {
-	return t.endTime
-}
-
-func (t *TaskExtension) ClearEndTime() {
-	t.endTime = nil
-}
-
-func (t *TaskExtension) SetTotalBytes(totalBytes int64) {
-	t.totalBytes = totalBytes
-}
-
-func (t *TaskExtension) GetTotalBytes() int64 {
-	return t.totalBytes
-}
-
-func (t *TaskExtension) Ctx() context.Context {
-	if t.ctx == nil {
-		t.ctxInitMutex.Lock()
-		if t.ctx == nil {
-			t.ctx = context.WithValue(t.Base.Ctx(), "user", t.Creator)
-		}
-		t.ctxInitMutex.Unlock()
-	}
-	return t.ctx
-}
-
-func (t *TaskExtension) ReinitCtx() {
-	if !conf.Conf.Tasks.AllowRetryCanceled {
-		return
-	}
-	select {
-	case <-t.Base.Ctx().Done():
-		ctx, cancel := context.WithCancel(context.Background())
-		t.SetCtx(ctx)
-		t.SetCancelFunc(cancel)
-		t.ctx = nil
-	default:
-	}
-}
-
-type TaskExtensionInfo interface {
+type TaskInfoWithCreator interface {
 	tache.TaskWithInfo
+	SetCreator(creator *model.User)
 	GetCreator() *model.User
-	GetStartTime() *time.Time
-	GetEndTime() *time.Time
-	GetTotalBytes() int64
 }
